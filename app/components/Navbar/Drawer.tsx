0ab2efcdc8e2041de034a917792055d8c91c1723
loremdev2 <singanamalaaman@gmail.com>
2025-01-19 11:26:23 +0530

    Replace img with next/image

--- app/components/Navbar/Drawer.tsx
+++ app/components/Navbar/Drawer.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
-import { XMarkIcon } from '@heroicons/react/24/outline'
+import Image from "next/image";
+import { XMarkIcon } from '@heroicons/react/24/outline';
 
 interface DrawerProps {
     children: ReactNode;
@@ -8,14 +9,13 @@ interface DrawerProps {
 }
 
 const Drawer = ({ children, isOpen, setIsOpen }: DrawerProps) => {
-
     return (
         <main
             className={
-                " fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out " +
+                "fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out " +
                 (isOpen
-                    ? " transition-opacity opacity-100 duration-500 translate-x-0  "
-                    : " transition-all delay-500 opacity-0 -translate-x-full  ")
+                    ? "transition-opacity opacity-100 duration-500 translate-x-0"
+                    : "transition-all delay-500 opacity-0 -translate-x-full")
             }
         >
             <section
@@ -24,32 +24,43 @@ const Drawer = ({ children, isOpen, setIsOpen }: DrawerProps) => {
                     (isOpen ? "translate-x-0" : "-translate-x-full")
                 }
             >
-
                 <article className="relative w-270 max-w-lg pb-10 flex flex-col space-y-6 h-full">
-                    <header className="p-4 flex items-center justify-between"><img
-                        className="h-12 w-40"
-                        src={"/assets/logo/logo.png"}
-                        alt="Courses-Logo"
+                    <header className="p-4 flex items-center justify-between">
+                        {/* Replace img with Image */}
+                        <Image
+                            className="cursor-pointer"
+                            src="/assets/logo/logo.png"
+                            alt="Courses-Logo"
+                            width={160} // Set the appropriate width
+                            height={48} // Set the appropriate height
+                            onClick={() => {
+                                setIsOpen(false);
+                            }}
+                        />
+                        <XMarkIcon
+                            className="block h-6 w-6 cursor-pointer"
+                            onClick={() => {
+                                setIsOpen(false);
+                            }}
+                        />
+                    </header>
+                    <div
                         onClick={() => {
                             setIsOpen(false);
                         }}
-                    /><XMarkIcon className="block h-6 w-6" onClick={() => {
-                        setIsOpen(false);
-                    }} />
-                    </header>
-                    <div onClick={() => {
-                        setIsOpen(false);
-                    }}>{children}</div>
+                    >
+                        {children}
+                    </div>
                 </article>
             </section>
             <section
-                className=" w-screen h-full cursor-pointer "
+                className="w-screen h-full cursor-pointer"
                 onClick={() => {
                     setIsOpen(false);
                 }}
             ></section>
         </main>
     );
-}
+};
 
 export default Drawer;
