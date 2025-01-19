d9ccf94bd6dd14132159759a892cfcecceee17a4
loremdev2 <singanamalaaman@gmail.com>
2025-01-19 19:32:06 +0530

    Refactor ExhibitorsPage to use external cards data and add a loading component

--- app/components/Exhibitors/ExhibitorsPage.tsx
+++ app/components/Exhibitors/ExhibitorsPage.tsx
@@ -1,78 +1,14 @@
 import React from "react";
 import ResponsiveCard from "./ResponsiveCard";
+import { cards } from "./cardsData";
 
 const ExhibitorsPage: React.FC = () => {
-    const cards = [
-        {
-            imageSrc: "/assets/ExhibitorImages/ayubal-wellness.png",
-            header: "AYUBAL WELLNESS",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        },
-        {
-            imageSrc: "/assets/ExhibitorImages/aiml.png",
-            header: "HERBAL PRODUCTS",
-            description:
-                "Discover a wide range of natural and herbal products crafted with care to promote health and wellness.",
-        },
-        {
-            imageSrc: "/assets/ExhibitorImages/uma_ayurveda.png",
-            header: "UMA AYURVEDA",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        },
-        {
-            imageSrc: "/assets/ExhibitorImages/zandu.png",
-            header: "ZANDU",
-            description:
-                "Discover a wide range of natural and herbal products crafted with care to promote health and wellness.",
-        },
-        {
-            imageSrc: "/assets/ExhibitorImages/THE_ARYA_VAIDYA_PHARMACY.png",
-            header: "THE ARYA VAIDYA PHARMACY",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        },
-        {
-            imageSrc: "/assets/ExhibitorImages/kairali.png",
-            header: "KAIRALI",
-            description:
-                "Discover a wide range of natural and herbal products crafted with care to promote health and wellness.",
-        },
-        {
-            imageSrc: "/assets/ExhibitorImages/pravek.png",
-            header: "PRAVEK",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        },
-        {
-            imageSrc: "/assets/ExhibitorImages/vitthal.png",
-            header: "VITTHAL",
-            description:
-                "Discover a wide range of natural and herbal products crafted with care to promote health and wellness.",
-        },
-        {
-            imageSrc: "/assets/ExhibitorImages/kayakalp.png",
-            header: "KAYAKALP",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        },
-        {
-            imageSrc: "/assets/ExhibitorImages/shree_mohta.png",
-            header: "SHREE MOHTA",
-            description:
-                "Discover a wide range of natural and herbal products crafted with care to promote health and wellness.",
-        },
-        {
-            imageSrc: "/assets/ExhibitorImages/sevasadan.png",
-            header: "SEVASADAN",
-            description:
-                "Discover a wide range of natural and herbal products crafted with care to promote health and wellness.",
-        }
-    ];
+
 
     return (
         <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center space-y-8 p-4">
+            <p className="text-2xl font-bold ">What We Do</p>
+            <div className="h-1 w-24 bg-orange-500 mx-auto"></div>
             {cards.map((card, index) => (
                 <ResponsiveCard
                     key={index}
---
+++ app/components/Exhibitors/cardsData.ts
@@ -0,0 +1,68 @@
+export const cards = [
+    {
+        imageSrc: "/assets/ExhibitorImages/ayubal-wellness.png",
+        header: "AYUBAL WELLNESS",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+        imageSrc: "/assets/ExhibitorImages/aiml.png",
+        header: "HERBAL PRODUCTS",
+        description:
+            "Discover a wide range of natural and herbal products crafted with care to promote health and wellness.",
+    },
+    {
+        imageSrc: "/assets/ExhibitorImages/uma_ayurveda.png",
+        header: "UMA AYURVEDA",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+        imageSrc: "/assets/ExhibitorImages/zandu.png",
+        header: "ZANDU",
+        description:
+            "Discover a wide range of natural and herbal products crafted with care to promote health and wellness.",
+    },
+    {
+        imageSrc: "/assets/ExhibitorImages/THE_ARYA_VAIDYA_PHARMACY.png",
+        header: "THE ARYA VAIDYA PHARMACY",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+        imageSrc: "/assets/ExhibitorImages/kairali.png",
+        header: "KAIRALI",
+        description:
+            "Discover a wide range of natural and herbal products crafted with care to promote health and wellness.",
+    },
+    {
+        imageSrc: "/assets/ExhibitorImages/pravek.png",
+        header: "PRAVEK",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+        imageSrc: "/assets/ExhibitorImages/vitthal.png",
+        header: "VITTHAL",
+        description:
+            "Discover a wide range of natural and herbal products crafted with care to promote health and wellness.",
+    },
+    {
+        imageSrc: "/assets/ExhibitorImages/kayakalp.png",
+        header: "KAYAKALP",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+        imageSrc: "/assets/ExhibitorImages/shree_mohta.png",
+        header: "SHREE MOHTA",
+        description:
+            "Discover a wide range of natural and herbal products crafted with care to promote health and wellness.",
+    },
+    {
+        imageSrc: "/assets/ExhibitorImages/sevasadan.png",
+        header: "SEVASADAN",
+        description:
+            "Discover a wide range of natural and herbal products crafted with care to promote health and wellness.",
+    }
+];
