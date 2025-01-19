12d29a4d19cf60ddbdc4e8a24195649e62ab4358
loremdev2 <singanamalaaman@gmail.com>
2025-01-19 11:54:28 +0530

    Fix Build

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <main>
+        <h1>Ayurmahotsava HomePage</h1>
 
       </main>
     </div>
   );
 }
+
+export default Home;
