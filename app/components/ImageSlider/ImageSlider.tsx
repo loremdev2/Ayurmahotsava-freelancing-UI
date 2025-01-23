a5b3aa86e553101ae29694a8fbffed4119128d4d
loremdev2 <singanamalaaman@gmail.com>
2025-01-24 02:21:50 +0530

    Enhance ImageSlider with dimmed image effect, overlay, and custom heading styles; adjust button size and positioning

--- app/components/ImageSlider/ImageSlider.tsx
+++ app/components/ImageSlider/ImageSlider.tsx
@@ -70,30 +70,34 @@ export default function ImageSlider(): any {
                 onMouseOver={handleMouseOver}
                 onMouseLeave={handleMouseLeave}
             >
+                {/* Dimmed Image with filter */}
                 <Image
                     src={images[currentIndex].src}
                     alt={`Slider Image ${currentIndex + 1}`}
                     layout="fill"
                     objectFit="cover"
-                    className="transition-all duration-500 ease-in-out cursor-pointer"
+                    className="transition-all duration-500 ease-in-out cursor-pointer opacity-80" // Apply dim effect here
                 />
+                {/* Overlay to darken the image */}
+                <div className="absolute top-0 left-0 w-full h-full bg-black opacity-30 z-0"></div>
+
                 {/* Centered Text */}
                 <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl font-bold z-10 text-heading">
                     {images[currentIndex].text}
                 </div>
+
                 {/* Heading with custom styles */}
-                <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 custom-heading">
+                <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 custom-heading text-white text-sm sm:text-xs lg:text-sm">
                     Heading
                 </div>
 
                 {/* Conditional "Read More" button on 2nd and 3rd images */}
                 {(currentIndex === 1 || currentIndex === 2) && (
                     <button
-                        className="absolute top-[calc(50%+20px)] left-1/2 transform -translate-x-1/2 px-4 py-1 border-2 border-white text-white text-lg rounded-full bg-transparent hover:bg-white hover:text-black transition-all duration-300 ease-in-out z-10"
+                        className="absolute top-[calc(50%+20px)] left-1/2 transform -translate-x-1/2 px-2 py-1 mt-6 border-2 border-white text-white text-sm sm:text-sm rounded-full bg-transparent hover:bg-white hover:text-black transition-all duration-300 ease-in-out z-10"
                     >
                         Read More
                     </button>
-
                 )}
 
                 <button
